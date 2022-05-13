59cc3c6a304933aa1cdd99fe63c67acddd263519
chandanbanerjee10 <chandanbanerjee514@gmail.com>
2022-05-13 21:47:34 +0530

    Added Delete User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
 
-    before_action :set_user, only: [:edit, :update, :show]
+    before_action :set_user, only: [:edit, :update, :show, :destroy]
     before_action :require_user, only: [:edit, :update]
-    before_action :require_same_user, only: [:edit, :update]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
 
     def new
         @user = User.new
@@ -37,6 +37,12 @@ class UsersController < ApplicationController
 
     def index
         @users = User.paginate(page: params[:page], per_page: 3)
+    end
+
+    def destroy
+        @user.destroy
+        session[:user_id] = nil
+        flash[:danger] = "Account and articles successfully deleted"
     end
 
     private
